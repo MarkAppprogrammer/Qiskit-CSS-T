84346592c29f72085bf8a337b62860c6e5fdbc16
NOT4LONG <js.jeong@yonsei.ac.kr>
2026-01-19 19:42:59 +0900

    .

--- decoder/cluster/run.sh
+++ decoder/cluster/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=Stim_MWPF
 #SBATCH --partition=normal
-#SBATCH --nodes=8
+#SBATCH --nodes=4
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=64
 #SBATCH --output=logs/slurm_%j.out
